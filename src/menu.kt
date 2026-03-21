9c85a10fd1fb26af89fa74c6233fb300694c2908
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-03-21 00:30:37 -0600

    menu.kt

---
+++ src/menu.kt
@@ -0,0 +1,39 @@
+fun main() {
+
+    val inventario = mutableListOf(
+        Producto("Laptop", 800.0, 5),
+        Producto("Mouse", 20.0, 10),
+        Producto("Teclado", 50.0, 7),
+        Producto("Monitor", 200.0, 3)
+    )
+
+    val carrito = mutableListOf<ItemCarrito>()
+
+    var opcion: Int
+
+    do {
+        println("\n====== MENÚ PRINCIPAL ======")
+        println("1. Ver productos")
+        println("2. Agregar producto")
+        println("3. Ver carrito")
+        println("4. Eliminar producto")
+        println("5. Comprar")
+        println("6. Salir")
+        print("Seleccione una opción: ")
+
+        opcion = readLine()!!.toInt()
+
+        when (opcion) {
+
+            1 -> mostrarInventario(inventario)
+            2 -> agregarProductoAlCarrito(carrito, inventario)
+            3 -> mostrarCarrito(carrito)
+            4 -> eliminarProductoDelCarrito(carrito)
+            5 -> realizarCompra(carrito)
+            6 -> println("Saliendo del sistema...")
+
+            else -> println("Opción inválida")
+        }
+
+    } while (opcion != 6)
+}
