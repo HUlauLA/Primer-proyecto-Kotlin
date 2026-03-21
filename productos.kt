f0c7908efd02dd68289ba3d95a1ef911c9ff65d8
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-03-21 00:56:54 -0600

    productos.kt

--- productos.kt
+++ productos.kt
@@ -18,31 +18,31 @@ fun mostrarInventario(productos: List<Producto>) {
     }
 }
 
-fun main() {
-    val scanner = Scanner(System.`in`)
-    val inventario = mutableListOf<Producto>()
+// fun main() {
+  //  val scanner = Scanner(System.`in`)
+   // val inventario = mutableListOf<Producto>()
 
-    print("¿Cuántos productos desea ingresar?: ")
-    val cantidadProductos = scanner.nextInt()
-    scanner.nextLine() // limpiar buffer
+ //   print("¿Cuántos productos desea ingresar?: ")
+   // val cantidadProductos = scanner.nextInt()
+  //  scanner.nextLine() // limpiar buffer
 
-    for (i in 1..cantidadProductos) {
-        println("\nProducto #$i")
+ //   for (i in 1..cantidadProductos) {
+     //   println("\nProducto #$i")
 
-        print("Nombre: ")
-        val nombre = scanner.nextLine()
+     //   print("Nombre: ")
+     //   val nombre = scanner.nextLine()
 
-        print("Precio: ")
-        val precio = scanner.nextDouble()
+      //  print("Precio: ")
+      //  val precio = scanner.nextDouble()
 
-        print("Cantidad disponible: ")
-        val cantidadDisponible = scanner.nextInt()
-        scanner.nextLine() // limpiar buffer
+       // print("Cantidad disponible: ")
+     //   val cantidadDisponible = scanner.nextInt()
+     //   scanner.nextLine() // limpiar buffer
 
-        val producto = Producto(nombre, precio, cantidadDisponible)
-        inventario.add(producto)
-    }
+   //     val producto = Producto(nombre, precio, cantidadDisponible)
+     //   inventario.add(producto)
+//    }
 
     // invoca la funcion mostrarInventario
-    mostrarInventario(inventario)
-}
+//    mostrarInventario(inventario)
+//}
